e1ced1e146a06058f2dfc72f34a123a54ce94834
iFamished <me@ifamished.com>
2026-07-06 22:43:50 +0000

    Horizon

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom"
 
 // Shared UI package styles
 import "ifamished-ui/styles/index.css"
-import "ifamished-ui/styles/themes/frost.css"
+import "ifamished-ui/styles/themes/horizon.css"
 
 // Site-specific styles
 import "./styles/background.css"
