715274e651764cfa5d3a57dd6370697cbcf079a1
iFamished <me@ifamished.com>
2026-07-06 23:01:42 +0000

    Eclipse

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom"
 
 // Shared UI package styles
 import "ifamished-ui/styles/index.css"
-import "ifamished-ui/styles/themes/dream.css"
+import "ifamished-ui/styles/themes/eclipse.css"
 
 // Site-specific styles
 import "./styles/background.css"
