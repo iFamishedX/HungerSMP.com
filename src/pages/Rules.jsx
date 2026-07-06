44fcea868853154f4d120a6d03272a38c19f3051
iFamished <me@ifamished.com>
2026-07-06 02:54:16 +0000

    Update

--- src/pages/Rules.jsx
+++ src/pages/Rules.jsx
@@ -112,9 +112,9 @@ const ruleSections = [
               </div>
               <h3>Not Allowed</h3>
             </div>
-              <p>No chat behavior is moderated.</p>
-              <p><strong>No Chat Reports</strong> is installed, but Mojang may still ban players for any reason, including chat messages.</p>
-              <p>Hunger SMP does not endorse or take responsibility for player chat messages.</p>
+              <p>Chat behavior is <strong>not moderated.</strong> 
+              <a href="https://modrinth.com/mod/no-chat-reports" target="_blank" rel="noopener noreferrer">No Chat Reports</a> is installed, but Mojang may still ban players for any reason, including chat messages.</p>
+              <p>The Hunger SMP does not endorse or take responsibility for player chat messages.</p>
           </GlassCard>
         </div>
       </>
