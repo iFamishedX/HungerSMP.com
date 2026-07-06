cf62034e9c24a26b3b14032bb5d0d5017d862802
iFamished <me@ifamished.com>
2026-07-06 22:36:18 +0000

    Sunset

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom"
 
 // Shared UI package styles
 import "ifamished-ui/styles/index.css"
-import "ifamished-ui/styles/themes/crystal.css"
+import "ifamished-ui/styles/themes/sunset.css"
 
 // Site-specific styles
 import "./styles/background.css"
