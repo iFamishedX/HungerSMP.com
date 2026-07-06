fe12ac7fd7e58acd38e3b033c5c2870c1bf5caed
iFamished <me@ifamished.com>
2026-07-06 22:52:38 +0000

    Aurora

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom"
 
 // Shared UI package styles
 import "ifamished-ui/styles/index.css"
-import "ifamished-ui/styles/themes/arc.css"
+import "ifamished-ui/styles/themes/aurora.css"
 
 // Site-specific styles
 import "./styles/background.css"
