0c19bf1956e1b4eca7a38bcb3703b8a4d978200c
iFamished <me@ifamished.com>
2026-07-07 01:29:31 +0000

    Synthwave

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom"
 
 // Shared UI package styles
 import "ifamished-ui/styles/index.css"
-import "ifamished-ui/styles/themes/premium/prism.css"
+import "ifamished-ui/styles/themes/premium/synthwave.css"
 
 // Site-specific styles
 import "./styles/background.css"
