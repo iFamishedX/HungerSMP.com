fa041e08003058fd57232a97978fc3f315187a88
iFamished <me@ifamished.com>
2026-07-07 14:46:25 +0000

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
-
-export default defineConfig({
-  plugins: [react(), cloudflare()],
-})
-
 import copyIfamishedThemes from "./vite.copyThemes.js";
 
-export default {
+export default defineConfig({
   plugins: [
+    react(),
+    cloudflare(),
     copyIfamishedThemes(),
   ],
-};
+});
