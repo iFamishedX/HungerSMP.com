a74a5f344f19d7a76d90bd6122a2c18a5b6a68f3
iFamished <me@ifamished.com>
2026-07-08 03:00:28 +0000

    Tundra

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom"
 
 // Shared UI package styles
 import "ifamished-ui/styles/index.css"
-import "ifamished-ui/styles/themes/cold-blue/glacier.css"
+import "ifamished-ui/styles/themes/cold-blue/tundra.css"
 
 // Site-specific styles
 import "./styles/background.css"
