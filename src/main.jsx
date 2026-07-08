d0717b24de3d298050b1a8f94017f23a158d1c68
iFamished <me@ifamished.com>
2026-07-08 03:12:11 +0000

    Aqua

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom"
 
 // Shared UI package styles
 import "ifamished-ui/styles/index.css"
-import "ifamished-ui/styles/themes/solid/violet.css"
+import "ifamished-ui/styles/themes/solid/aqua.css"
 
 // Site-specific styles
 import "./styles/background.css"
